f0261b0ea6e88fa21febe5320767e55542f75494
EWSoftware <Eric@EWoodruff.us>
2025-04-20 13:14:01 -0700

    Removed VS2019 support
    - Removed VS2019 support.
    - Enabled VSIX ARM64 installation target.

--- Source/ExtendedDocCommentsProvider2022/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:EWSoftware.CompletionProviders.CSharpXmlDocCommentsCompletionProvider.TriggerOnTypingLetters(Microsoft.CodeAnalysis.Options.OptionSet)~System.Boolean")]
-[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:EWSoftware.CompletionProviders.CSharpXmlDocCommentsCompletionProvider.GetItemsAsync(Microsoft.CodeAnalysis.Document,System.Int32,Microsoft.CodeAnalysis.Completion.CompletionTrigger,System.Threading.CancellationToken)~System.Threading.Tasks.Task{System.Collections.Generic.IEnumerable{Microsoft.CodeAnalysis.Completion.CompletionItem}}")]
